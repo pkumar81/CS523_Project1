53ff87a843acf59bf582255e57abd11cd332ac3b
MengqinCai <mcai1988@unm.edu>
2017-02-09 11:35:54 -0700

    Update script_eigenvalue_func.m

--- script_eigenvalue_func.m
+++ script_eigenvalue_func.m
@@ -1,7 +1,7 @@
 F = @(xy,a, b) [a-xy(1)^2+b*xy(2);xy(1)] % Duffling Map
 F_Jacobian = @(xy,a, b) [-2*xy(1) b;1 0] % Duffling Jacobian
 max_time = 500; % How long to run (= number of samples to average)
-rho_range= [0.2 0.37 0.4 0.91 0.10 0.102 1.03 1.05 1.2 1.23 1.25 1.26]; % Range over parameter 1: (a for Duffling Map)
+rho_range= [0.2 0.368 0.784 0.911 0.978 0.1025 1.039 1.051 1.2 1.23 1.231 1.26]; % Range over parameter 1: (a for Duffling Map)
 gammar = 0.3; % Fix parameter 2 (b for Duffling Map)
 % Initial values for x and y
 x0 = 0.3;
